cdffe990b72907e411dbe8bd1d59f6504c71a4c4
ArtemSermyagin <artem.sermyagin@yahoo.com>
2024-02-23 14:51:53 +0300

    создал миграцию и json файл с фикстурой

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, verbose_name="Наименование")
-    descriptions = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return f"{self.id}"
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Наименование")
-    descriptions = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание")
     image = models.ImageField(
         upload_to="products/",
         verbose_name="Изображение"
