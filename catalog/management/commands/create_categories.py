6962e02f95085e3c33883021b05f14e43a2d9791
ArtemSermyagin <artem.sermyagin@yahoo.com>
2024-02-25 12:12:25 +0300

    6 задание

--- catalog/management/commands/create_categories.py
+++ catalog/management/commands/create_categories.py
@@ -1,42 +1,46 @@
+import json
+
 from django.core.management import BaseCommand
+
+from catalog.models import Category, Product
+from config.settings import BASE_DIR
 
 
 class Command(BaseCommand):
+    JSON_NAME = "db.json"
 
-    @staticmethod
-    def json_read_categories():
-        pass
+    @classmethod
+    def path(cls):
+        return f"{BASE_DIR}/fixtures/{cls.JSON_NAME}"
 
-    @staticmethod
-    def json_read_products():
-        pass
+    @classmethod
+    def json_read_categories(cls):
+        with open(cls.path(), encoding="utf-8") as file:
+            return [item for item in json.load(file) if item["model"] == "catalog.category"]
+
+    @classmethod
+    def json_read_products(cls):
+        with open(cls.path(), encoding="utf-8") as file:
+            return [item for item in json.load(file) if item["model"] == "catalog.product"]
+
     def handle(self, *args, **options):
-
-        # Удалите все продукты
-				# Удалите все категории
-
-				# Создайте списки для хранения объектов
+        Category.objects.all().delete()
+        Product.objects.all().delete()
         product_for_create = []
         category_for_create = []
 
-				# Обходим все значения категорий из фиктсуры для получения информации об одном объекте
         for category in Command.json_read_categories():
             category_for_create.append(
-                Category(название_поля=значение_из_словаря, ..., название_поля=значение_из_словаря)
+                Category(**category['fields'])
             )
-
-				# Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
-				# Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
         for product in Command.json_read_products():
+            category_id = product['fields'].pop('category')
             product_for_create.append(
-                Product(название_поля=значение_из_словаря, ...,
-												# получаем категорию из базы данных для корректной связки объектов
-                        поле_категории=Category.objects.get(pk=значение_из_словаря), ...,
-                        название_поля=значение_из_словаря)
+                Product(
+                    **product['fields'],
+                    category=Category.objects.get(id=category_id)
+                )
             )
-
-				# Создаем объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
-
